1d0dc21dd988ead883ca3f02e08fc129149786a5
Jacek Generowicz <jacg@my-post-office.net>
2021-02-23 18:00:41 +0100

    Use flake-utils to generalize system

--- flake.nix
+++ flake.nix
@@ -2,30 +2,32 @@
 
   description = "A simple example of managing a project with a flake";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-20.09";
+  inputs.nixpkgs    .url = "github:NixOS/nixpkgs/nixos-20.09";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }:
-    let system = "x86_64-linux";
-        pkgs = import nixpkgs { inherit system; };
-    in {
-      packages.${system}.aaa-and-bbb = pkgs.stdenv.mkDerivation {
-        name = "flake-learning-curve";
-        src = self;
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; }; in
+      {
+        packages.aaa-and-bbb = pkgs.stdenv.mkDerivation {
+          name = "flake-learning-curve";
+          src = self;
 
-        buildInputs = [
-          pkgs.mdbook
-        ];
+          buildInputs = [
+            pkgs.mdbook
+          ];
 
-        buildPhase = ''
-          $CC -o aaa src/aaa.c
-          $CC -o bbb src/bbb.c
-        '';
+          buildPhase = ''
+            $CC -o aaa src/aaa.c
+            $CC -o bbb src/bbb.c
+          '';
 
-        installPhase = ''
-          mkdir -p $out/bin
-          install -t $out/bin aaa bbb
-        '';
+          installPhase = ''
+            mkdir -p $out/bin
+            install -t $out/bin aaa bbb
+          '';
 
-      };
-    };
+        };
+
+      });
 }
--- spec/nix_spec.sh
+++ spec/nix_spec.sh
@@ -1,9 +1,3 @@
-# The current flake is rather unsatisfactory: it only works on one system
-# (x86_64-linux) and the various nix commands (`nix shell`, `nix build`, etc.)
-# require verbose and inconvenient specification of the output we wish to
-# access. We will significantly improve on this in the next step, so let's not
-# dwell on it for too long. Here are two basic sanity checks:
-
 Describe 'nix'
 
 # --------------------------------------------------------------------------------
@@ -15,10 +9,70 @@ Describe 'nix'
 
 # --------------------------------------------------------------------------------
 
-  It 'nix shell --command'
-    When call nix shell .#packages.x86_64-linux.aaa-and-bbb --command aaa
+  # # TODO how to inspect something inside the shell?
+  # It 'shell (local default)'
+  #   When call nix shell
+  #   The output should equal 'This is program AAA.'
+  # End
+
+# --------------------------------------------------------------------------------
+  It 'shell local-flake#package-name --command'
+    # `nix shell --command <executable>` can be used within the repository (or
+    # anywhere on the local filesystem) to run executables installed by the
+    # `defaultPackage`.
+    When call nix shell .#aaa-and-bbb --command aaa
     The output should equal 'This is program AAA.'
   End
+
+  It 'shell remote-flake#pakcage-name --command'
+    # It also works for flakes which are not present on the local machine.
+    When call nix shell github:jacg/flakes-learning-curve/generalize-system#aaa-and-bbb --command bbb
+    The output should include 'this is program BBB!'
+  End
+
+  # TODO point out that `-c 'this | that'` doesn't work, and the `-c bash -c
+  # 'this | that'` workaround.
+
+# --------------------------------------------------------------------------------
+
+  It 'nix build'
+    When call nix build
+    The status should be failure
+    The error should include 'does not provide attribute'
+    The error should include 'defaultPackage'
+  End
+
+  # --------------------------------------------------------------------------------
+
+  It 'nix shell'
+  When call nix shell
+  The status should be failure
+  The error should include 'does not provide attribute'
+  The error should include 'defaultPackage'
+  End
+
+# --------------------------------------------------------------------------------
+
+  It 'nix develop'
+    When call nix develop
+    The status should be failure
+    The error should include 'does not provide attribute'
+    The error should include 'defaultPackage'
+  End
+
+# --------------------------------------------------------------------------------
+  It 'run'
+    # By default `nix run` tries to execute a binary with a name that is
+    # identical to the name of the `default Package`. Our current flake does not
+    # include such a binary, so `nix run` will fail:
+    When call nix run
+    The status should be failure
+    The error should include 'cannot find flake attribute'
+    The error should include "#defaultApp."
+  End
+  # Next we'll see two approaches to getting this to work:
+  # 1. Set a binary with the appropritate name.
+  # 2. Define a `defaultApp` in the flake's outputs.
 
 # --------------------------------------------------------------------------------
 
